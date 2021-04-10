786d96f85bf2dcf6a5eea3c334940955cd8625a1
Diego de Crassus Ferreira Batista Neves <diego.neves@iesb.edu.br>
2021-04-10 15:16:44 -0300

    MOD: esqueci as telas!

--- app/app/src/main/java/br/com/PDM/MainActivity.kt
+++ app/app/src/main/java/br/com/PDM/MainActivity.kt
@@ -1,11 +1,23 @@
 package br.com.PDM
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this, signIn::class.java))
+        }
+
+        btnCadastro.setOnClickListener {
+            startActivity(Intent(this, signUp::class.java))
+        }
     }
+
+
 }
