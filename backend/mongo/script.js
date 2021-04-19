583bce32ea3cca680fec1020053d54a8e5d6449a
Diego de Crassus Ferreira Batista Neves <diego.neves@iesb.edu.br>
2021-04-19 12:16:41 -0300

    remoção da inicialização do script

--- backend/mongo/script.js
+++ backend/mongo/script.js
@@ -1,10 +1,10 @@
 print('Start #################################################################');
-var document = {
-    "user": 'teste@teste.com',
-    "face_encode": [0, 0],
-    "name": "teste2",
-    "image": "base64"
-};
+// var document = {
+//     "user": 'teste@teste.com',
+//     "face_encode": [0, 0],
+//     "name": "teste2",
+//     "image": "base64"
+// };
 
 db.PDM.insert(document);
 print('END #################################################################');
