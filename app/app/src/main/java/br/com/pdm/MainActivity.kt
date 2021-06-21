bb74fdbf8f889a340db2f9293ec515b804f8772a
saraselis <saras1@hotmail.com.br>
2021-06-21 20:40:42 -0300

    RM: navigation

--- app/app/src/main/java/br/com/pdm/MainActivity.kt
+++ app/app/src/main/java/br/com/pdm/MainActivity.kt
@@ -1,11 +1,8 @@
 package br.com.pdm
 
 import android.content.Intent
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,36 +17,10 @@ class MainActivity : AppCompatActivity() {
         btnCadastro.setOnClickListener {
             startActivity(Intent(this, signUp::class.java))
         }
-    }
 
-    override fun onStart() {
-        super.onStart()
-        Toast.makeText(applicationContext, "onStart", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Toast.makeText(applicationContext, "onResume", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Toast.makeText(applicationContext, "onPause", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Toast.makeText(applicationContext, "onStop", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Toast.makeText(applicationContext, "onRestart", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Toast.makeText(applicationContext, "onDestroy", Toast.LENGTH_SHORT).show()
+        btCarrossel.setOnClickListener {
+            startActivity(Intent(this, MainActivityCarrossel::class.java))
+        }
     }
 
 }
--- app/app/src/main/java/br/com/pdm/MainActivityCarrossel.kt
+++ app/app/src/main/java/br/com/pdm/MainActivityCarrossel.kt
@@ -1,9 +1,7 @@
 package br.com.pdm
 
-import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -13,7 +11,6 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.size
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
-import br.com.pdm.ui.fragment.MainFragment
 import kotlinx.android.synthetic.main.activity_main_carrossel.*
 import kotlinx.android.synthetic.main.tip_content.view.*
 import java.lang.Math.abs
@@ -64,7 +61,7 @@ class MainActivityCarrossel : AppCompatActivity() {
         btBack.setOnClickListener{
             if (view_pager.currentItem == view_pager.size)
                 Toast.makeText(this, "Retornando ao menu principal...", Toast.LENGTH_LONG).show()
-                val intent=Intent(this@MainActivityCarrossel, br.com.pdm.ui.fragment.MainFragment::class.java)
+                val intent=Intent(this@MainActivityCarrossel, br.com.pdm.MainActivity::class.java)
                 startActivity(intent)
 
             view_pager.setCurrentItem(view_pager.currentItem - 1, true)
